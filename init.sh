372ccfa5b5e464a610895ce1a7aa2ee0af8c630c
rinex20 <rinex20@me.com>
2022-11-17 00:29:17 +0800

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# adjust fan speed to reduce the fan noise
+# monitor cpu temp. if the temp. > 60 , set fan speed to high mode
+# otherwise keep low speed 
+
+# install docker
+
+
+# install tightvnc
+# https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-debian-11
+# fix issue of missing "fixed" font
+# apt-get install xfonts-base
+
+
+# install gpsd from source
+
+
+# install rtkrcv and str2str
+# config file
+# systemd service
+
+
+# fix xgps issue
+# apt install python3-gi-cairo or python-gi-cairo
+# or reinstall: apt install --reinstall python3-gi-cairo
+
