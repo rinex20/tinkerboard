63218775d24d4b5a1fd84e02c38e214962086952
rinex20 <rinex20@me.com>
2022-11-17 00:34:23 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,7 +5,21 @@
 # otherwise keep low speed 
 
 # install docker
+# Install Python dependencies.
+sudo python3 -m pip install pip --upgrade
 
+sudo apt-get update
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] \
+https://download.docker.com/linux/debian \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo update-alternatives --set iptables /usr/sbin/iptables-legacy
+sudo update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
 
 # install tightvnc
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-vnc-on-debian-11
